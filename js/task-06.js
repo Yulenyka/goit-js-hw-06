6051a5e0237b06b618943b83a94aaf00b9eecfc1
Юлия Пшеничная <yulenykaa@gmail.com>
2022-10-14 11:07:55 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -2,12 +2,16 @@ const textInput = document.querySelector("#validation-input");
 const textLenght = textInput.getAttribute("data-length");
 
 textInput.addEventListener("blur", (event) => {
-    if (textLenght > event.target.value.length) {
-      textInput.classList.remove("valid");
-      textInput.classList.add("invalid");
-    } else {
-      textInput.classList.remove("invalid");
+    if (textLenght == event.target.value.length) {
       textInput.classList.add("valid");
+      if (textInput.classList.contains("invalid")) {
+        textInput.classList.remove("invalid");
+      }
+    } else {
+      if (textInput.classList.contains("valid")) {
+        textInput.classList.remove("valid");
+      }
+      textInput.classList.add("invalid");
     }
 });
 
